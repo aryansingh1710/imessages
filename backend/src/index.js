d0c7e5f25f1d243c291120b13b4666bd7c914a8f
aryansingh171019 <aryansingh171019@gmail.com>
2026-06-28 11:29:59 +0530

    Add user and message models, implement MongoDB connection, and update server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();  
 import express from "express";
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
+import { connectDB } from "./lib/db.js";
+import User from "./models/user.model.js";
 
-const app=express();
-const PORT=process.env.PORT;
+const app = express();
 
-app.listen(PORT,()=>
-                console.log("Server is up and running on port:", PORT));
+const PORT = process.env.PORT || 3000;
+const FRONTEND_URL=process.env.FRONTEND_URL
 
+app.use(clerkMiddleware())
 
+app.use(cors({origin:FRONTEND_URL,credentials:true}))
+
+app.use(express.json())
+
+app.get("/health",(req,res)=>{
+    res.status(200).json({ok:true})
+})
+
+app.listen(PORT, async () => {
+    await connectDB();  
+    console.log(`Server is up and running on PORT: ${PORT}`);
+});
+ 
